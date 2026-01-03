ab15226cbf98546cf6f736257bb5db9a5fb7cee2
Adam <blacksheep9779@163.com>
2026-01-03 12:17:11 +0800

    对话框层叠样式处理(z-index)

--- localfts-server/src/main/resources/static/common.js
+++ localfts-server/src/main/resources/static/common.js
@@ -10,6 +10,13 @@ function setClass(id, cn) {
     }
 }
 
+function setStyle(id,styleKey,styleValue) {
+    var element = document.getElementById(id);
+    if(element) {
+        element.style[styleKey] = styleValue;
+    }
+}
+
 function updateTitle(id, text, color) {
     var element = document.getElementById(id);
     if(element) {
@@ -21,4 +28,54 @@ function updateTitle(id, text, color) {
             element.style.color = 'black';
         }
     }
+}
+
+function getElementsByClassName(className, tagName) {
+    if(document.getElementsByClassName) {
+        return document.getElementsByClassName(className);
+    }
+    var elements = document.getElementsByTagName(tagName === undefined ? '*' : tagName);
+    var result = [];
+    var resultIndex = 0;
+    for(var i=0;i<elements.length;i++) {
+        var element = elements[i];
+        var elementClassName = element.className;
+        var cnArr = elementClassName.split(' ');
+        for(var j=0;j<cnArr.length;j++) {
+            if(cnArr[j] === className) {
+                result[resultIndex++] = element;
+                break;
+            }
+        }
+    }
+    return result;
+}
+
+function hasClass(element,className) {
+    if(!element) {
+        return false;
+    }
+    var cnArr = element.className.split(' ');
+    for(var i=0;i<cnArr.length;i++) {
+        var cn = cnArr[i];
+        if(cn === className) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function mergeCollectionOrArray(object1,object2) {
+    var result = [];
+    if(object1) {
+        for(var i=0;i<object1.length;i++) {
+            result.push(object1[i]);
+        }
+    }
+    if(object2) {
+        for(var i=0;i<object2.length;i++) {
+            result.push(object2[i]);
+        }
+    }
+    return result;
 }
---
+++ localfts-server/src/main/resources/static/dialog.js
@@ -0,0 +1,53 @@
+//Requires common.css
+//Requires common.js
+
+var zIndex = 1;
+var zIndexObject = {};
+
+function showDialog(id) {
+    var element = document.getElementById(id);
+    if(element) {
+        var hzid = getHighestZIndexDialog();
+        if(hzid != element) {
+            zIndexObject[id] = zIndex;
+            element.style.zIndex = zIndex;
+            zIndex++;
+        }
+        setClass(id, 'show');
+    }
+}
+
+function hideDialog(id) {
+    var element = document.getElementById(id);
+    if(element) {
+        delete zIndexObject[id];
+        /*var hasKey = false;
+        for(var key in zIndexObject) {
+            if(zIndexObject.hasOwnProperty(key)) {
+                hasKey = true;
+                break;
+            }
+        }
+        if(!hasKey) {
+            zIndex = 1;
+        }*/
+        setClass(id, 'hidden');
+    }
+}
+
+function getHighestZIndexDialog() {
+    var showDialogs = getElementsByClassName('show', 'div');
+    var hiddenDialogs = getElementsByClassName('hidden', 'div');
+    var dialogs = mergeCollectionOrArray(showDialogs, hiddenDialogs);
+    if(dialogs && dialogs.length > 0) {
+        var result = dialogs[0];
+        for(var i=1;i<dialogs.length;i++) {
+            var showDialog = dialogs[i];
+            if(showDialog.style.zIndex > result.style.zIndex) {
+                result = showDialog;
+            }
+        }
+        return result;
+    }
+    return null;
+}
