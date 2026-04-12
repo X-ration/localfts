ea886a5f06f1e7d1d544578853480900742359e1
Adam <starksmile@163.com>
2026-04-12 19:05:58 +0800

    将默认配置转移到java类中

--- localfts-server/src/main/java/com/adam/localfts/webserver/LocalFtsWebServer.java
+++ localfts-server/src/main/java/com/adam/localfts/webserver/LocalFtsWebServer.java
@@ -4,10 +4,13 @@ import com.adam.localfts.webserver.config.properties.LocalFtsProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.ansi.AnsiOutput;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ErrorProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-import java.net.SocketException;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @EnableConfigurationProperties(LocalFtsProperties.class)
@@ -15,9 +18,22 @@ public class LocalFtsWebServer {
 
 	private static final Logger logger = LoggerFactory.getLogger(LocalFtsWebServer.class);
 
-	public static void main(String[] args) throws SocketException {
-		SpringApplication.run(LocalFtsWebServer.class, args);
+	public static void main(String[] args) {
+		SpringApplication springApplication = new SpringApplication(LocalFtsWebServer.class);
+		setDefaultProperties(springApplication);
+		springApplication.run(args);
 	}
 
+	private static void setDefaultProperties(SpringApplication springApplication) {
+		Map<String, Object> defaultProperties = new HashMap<>();
+		defaultProperties.put("server.error.include-exception",true);
+		defaultProperties.put("server.error.include-stack-trace", ErrorProperties.IncludeStacktrace.ALWAYS);
+		defaultProperties.put("server.error.whitelabel.enabled", false);
+		defaultProperties.put("spring.output.ansi.enabled", AnsiOutput.Enabled.ALWAYS);
+		defaultProperties.put("spring.thymeleaf.cache", false);
+		defaultProperties.put("spring.mvc.static-path-pattern", "/static/**");
+		defaultProperties.put("spring.resources.static-locations", "classpath:/resources");
+		springApplication.setDefaultProperties(defaultProperties);
+	}
 
 }
